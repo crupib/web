b0b1b663f7b4b37274784237bd36e9ed1e6bcae7
root <crupib@gmail.com>
2023-03-30 15:12:25 -0400

    add script file

---
+++ myFunction.js
@@ -0,0 +1,9 @@
+function calculateAge(yearOfBirth)
+                {
+                        return(2023-yearOfBirth)
+                }
+
+        let myAge = calculateAge(1956)
+        
+        console.log("running age calculation");
+        console.log("I am " +myAge+ " years old");
